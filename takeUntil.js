a9237a25acaf39086a0d116f8023f2194c7b27e1
Evander Owusu <owu.evander@gmail.com>
2021-11-25 22:57:05 +0000

    create takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,33 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false 
+  } 
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertation Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🔴 Assertation Failed: ${arr1} !== ${arr2}`)
+  }
+  
+}
+
+
+
+
+
+
 const takeUntil = function(array, callback) {
   let results = []
   for (let element of array) {
