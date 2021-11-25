200dc15f018cba86a65dd095d67f0f2c6e1a5d3a
Evander Owusu <owu.evander@gmail.com>
2021-11-25 02:33:09 +0000

    createKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, value) {
+  console.log(Object.keys(object));
+  for (item of Object.keys(object)) {
+    if (object[item] === value) {
+      return item
+    }
+  }
+  
+}
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
