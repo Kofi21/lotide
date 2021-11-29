ffdcbc4bb7753064b3cc9cb0889a1ac4e01a9bcf
Evander Owusu <owu.evander@gmail.com>
2021-11-29 19:37:37 +0000

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed");
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
-    return false 
-  } 
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
       if (!eqArrays(array1[i], array2[i])) {
         return false;
       }
     } else if (array1[i] !== array2[i]) {
-      return false
+      return false;
     }
   }
   return true;
-}
+};
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]))
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
